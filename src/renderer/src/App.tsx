99fe9ac9c51a7c8aa59187c84438cbe4523c398d
manuelsrleon <manuelsrleon@gmail.com>
2025-04-13 19:05:07 +0200

    half-commit, add half scenarioMapper

--- src/renderer/src/App.tsx
+++ src/renderer/src/App.tsx
@@ -2,6 +2,7 @@ import { useEffect, useState } from 'react'
 import type Scenario from './model/scenario/scenario';
 import ScenarioBanner from './components/ScenarioBanner';
 import "./global.css";
+import ChallengeData from './model/scenario/challenge-data';
 
 
 function App(): JSX.Element {
@@ -13,13 +14,41 @@ function App(): JSX.Element {
   // })
   const loadSavedScenarios = async () => {
     try {
-      window.electron.loadSavedScenarios().then(
-        (data) => { console.log(data); setScenarios(data) }
-      );
-
+      const rawData = await window.electron.loadSavedScenarios();
+      const mappedScenarios: Scenario[] = rawData.map(
+      )
     } catch (err: any) {
       setError(err.message);
     }
+  }
+
+  type ScenarioQueryResult = {
+    type: string;
+    title: string;
+    subtitle: string;
+    challenge_prompt: string;
+    challenge_mediaPath: string;
+    challenge_maxTime: number;
+    challenge_rfidObjectCode: string;
+    success_text: string;
+    success_mediaPath: string;
+    success_soundPath: string;
+    failure_text: string;
+    failure_mediaPath: string;
+    failure_soundPath: string;
+  }
+  const scenarioQueryResultToScenarioMapper = async (scenarioQueryResult: ScenarioQueryResult) => {
+    let mappedScenario: Scenario = {} as Scenario;
+    mappedScenario = {
+      type: scenarioQueryResult.type,
+      title: scenarioQueryResult.title,
+      subtitle: scenarioQueryResult.subtitle,
+      challengeData: {} as ChallengeData
+    }
+
+    return {
+
+    } as Scenario;
   }
   return (
     <>
