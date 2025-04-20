1b152e6075d72a1a9553e5e1664fc543106b801a
manuelsrleon <manuelsrleon@gmail.com>
2025-04-20 14:42:19 +0200

    scenario page template implementation

--- src/renderer/src/pages/NewScenarioPage.tsx
+++ src/renderer/src/pages/NewScenarioPage.tsx
@@ -1,14 +1,24 @@
 import { useState } from "react";
-import "./scenarioPage.css"
+import "./newScenarioPage.css"
 import Scenario from "src/common/model/scenario/scenario";
 
 export const NewScenarioPage = () => {
 
-    const newScenario = useState<Scenario>();
+    const [newScenario, setNewScenario] = useState<Scenario>();
     
+
+    const [formData, setFormData] = useState<Scenario>({} as Scenario);
+    
+    const handleChange = (e) => {
+
+    }
     const validateScenario = (formData: any) => {
     }
     
+
+    const changeMaxTime = (quantity: number) => {
+        
+    }
     const submitNewScenario = () => {
         // if(validateScenario(newScenario)){
         //     window.electron.createNewScenario();
@@ -19,21 +29,52 @@ export const NewScenarioPage = () => {
         <h2>New Scenario</h2>
         <form action="submit" className="dd-form">
             <label htmlFor="scenarioTitle">
-                Nombre de escenario
+                Título de escenario
             </label>
             <input id="scenarioTitle" type="text" />
             <label htmlFor="scenarioSubtitle">
+                Subtítulo de escenario
             </label>
-            <label htmlFor="scenarioSubtitle">
-            </label>
-            <label htmlFor="maxTime">
-                Tiempo máximo
-            </label>
-            <input type="text" placeholder="0">
-            
-            </input>
+            <input type="text" id="scenarioSubtitle" />
+
+            <div className="scenario-specific-fields">
+                <div className="new-scenario-section dd-card">
+                    <h3>Desafío</h3>
+                    <label htmlFor="challengePrompt">
+                        Pregunta de desafío
+                    </label>
+                    <input type="text" placeholder="¿Qué situación se propone?"/>
+                    <label htmlFor="maxTime">
+                        Tiempo máximo (s)
+                    </label>
+                    <div className="dd-number-picker">
+                        <input id="maxTime" type="number" placeholder="0">
+                        </input>
+                    </div>
+                    <label htmlFor="rfidObjectCode">
+                        Objeto de la solución
+                    </label>
+                </div>
+                <div className="new-scenario-section dd-card">
+                    <h3>Al fallar...</h3>
+                    <label htmlFor="failureText">
+                        Texto al fallar
+                    </label>
+                    <input type="text" placeholder="Oops, ¡eso no es!"/>
+                
+                </div>
+                <div className="new-scenario-section dd-card">
+                    <h3>Al superarlo...</h3>
+                    <label htmlFor="successText">
+                        Texto al ganar
+                    </label>
+                    <input type="text" placeholder="¡Enhorabuena!"/>
+                </div>
+            </div>
+            <button onClick={submitNewScenario}>
+                {"Guardar"}
+            </button>
         </form>
-        <input type="text" />
         {/*
         Título de escenario, subtítulo de escenario, tipo de escenario (default = STANDARD)
         
@@ -49,8 +90,5 @@ export const NewScenarioPage = () => {
         */
         
         }
-        <button onClick={submitNewScenario}>
-
-        </button>
         </div>
 }
